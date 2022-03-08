bc796e820dad2bc1d619e93be610a342dac2422b
Piyush <bhangalepiyush@gmail.com>
2022-03-08 19:04:29 +0530

    add windi to config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import WindiCss from "vite-plugin-windicss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte(), WindiCss()],
+});
