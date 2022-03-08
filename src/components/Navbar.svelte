2005803bcbe86f18ab0c7b68eea9877eabd95b49
Piyush Bhangale <bhangalepiyush@gmail.com>
2022-03-08 19:30:53 +0530

    feat: navbar

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,18 @@
+<script></script>
+
+<div class="py-4 border-b-2 border-gray-800">
+  <div class="flex justify-between items-center">
+    <div>
+      <span class="font-poppins text-xl font-black tracking-wide">
+        AntraBin
+      </span>
+    </div>
+
+    <div>
+      <button
+        class="py-1 px-4 font-bold tracking-wide uppercase bg-green-400/40 text-green-700 hover:(bg-green-400/80 text-green-900) rounded font-poppins text-md"
+        >Create</button
+      >
+    </div>
+  </div>
+</div>
